d7fa649855fd285fff6fc42c6e260c3855da81f6
Ajay Mungur <ajaymungur@hotmail.com>
2021-06-26 01:13:18 +0100

    Reworked server to decompress the while game folder in the browser

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const compression = require("compression");
 const app = express();
 const port = process.env.PORT || 8989;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/game/index.html')
-})
+app.use(compression())
+app.use(express.static(__dirname + '/game/'))
+// app.get('/', (req, res) => {
+//   res.send(__dirname + '/game/index.html')
+// })
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`)
